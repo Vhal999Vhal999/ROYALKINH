0cc67869cb7336b70b7b7d752264b210727fb6d7
Vhal999Vhal999 <107368993+Vhal999Vhal999@users.noreply.github.com>
2025-03-04 14:22:02 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -68,10 +68,10 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/II_ROYALENTRY1128_II"
+          #      text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/II_ROYALENTRY1128_II"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/royalmusicboto"
+          #      text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/royalmusicboto"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
