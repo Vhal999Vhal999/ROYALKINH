cea1e0097d71dbaea86a509c0fb04a3ea841e93b
Vhal999Vhal999 <107368993+Vhal999Vhal999@users.noreply.github.com>
2025-03-04 14:19:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Vhal999Vhal999/Queenbotmusic",
+    "https://github.com/Vhal999Vhal999/ROYALKINH",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
