0af88c1a0e40236f9ad10e7a86789e3e1f07cc7e
Cookies Are On Github <107368993+Vhal999Vhal999@users.noreply.github.com>
2025-04-30 19:10:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/royalmusicboto")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ll_DPZ_WORLDS_ll")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/queenbotgrup")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
